e01ece1e35bdec70052171f8166adae10d31b571
Noah <45659684+NoahDrucker@users.noreply.github.com>
2023-10-10 15:14:45 +0300

    Update tiktokapihashtag.py

--- python/tiktokapihashtag.py
+++ python/tiktokapihashtag.py
@@ -17,7 +17,7 @@ username = 'username'
 password = 'password'
   
 response = requests.post(
-    'https://scraper-api.smartproxy.com/v1/scrape',
+    'https://scraper-api.smartproxy.com/v2/scrape',
     headers = headers,
     json = task_params,
     auth = (username, password)
